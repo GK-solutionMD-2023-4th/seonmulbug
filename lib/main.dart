6243cd0d0af578e9294e76c2fb4c74339d3a128b
rookie888888 <redfish8@naver.com>
2023-12-18 15:38:14 +0900

    메시지카드

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 //import 'package:seonmulbug/wishlist.dart';
-import 'package:seonmulbug/fundinginput.dart';
+//import 'package:seonmulbug/fundinginput.dart';
+import 'rookie2.dart';
 
 
 
 void main() {
-  runApp(MyApp2());
+  runApp(MyApp());
 }
