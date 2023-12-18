8014b6ec84d400c3226be5b7bbf7cf25652e18d8
rookie888888 <redfish8@naver.com>
2023-12-18 13:20:37 +0900

    ListView

--- lib/rookie2.dart
+++ lib/rookie2.dart
@@ -1,33 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
@@ -38,16 +19,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -70,20 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Colors.white,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         leading: IconButton(icon: Icon(Icons.arrow_back_ios_new), onPressed: null),
 
@@ -91,6 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
       ListView(
+        shrinkWrap: true,
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
           children: [
@@ -208,21 +169,18 @@ class _MyHomePageState extends State<MyHomePage> {
                  color: const Color.fromARGB(255, 223, 223, 223),
                  ),
                 ),
-
            ],//children
           )
           ],//children
         ),
-
-
         ],
       ),
 
       floatingActionButton: FloatingActionButton(
-       onPressed: () {
+      child: Text('test'),//Icon(Icons.add),
+      onPressed: () {
       // Handle the button press
       },
-      child: Icon(Icons.add),
       ),
 
      
