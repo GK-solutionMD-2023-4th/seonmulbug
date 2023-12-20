2993555c11ecc39746c9d55d0b45e41adb3f7ce1
rookie888888 <redfish8@naver.com>
2023-12-20 12:38:21 +0900

    루키

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:seonmulbug/wishlist.dart';
 //import 'package:seonmulbug/fundinginput.dart';
 //import 'package:seonmulbug/fundingfinish.dart';
-//import 'rookie4.dart';
+import 'rookie4.dart';
 //import 'rookie3.dart';
 import 'package:seonmulbug/givecard.dart';
 
